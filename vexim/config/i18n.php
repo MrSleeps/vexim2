6e19dd29cc2895d9badb83bd826c31ef3bd952be
Mr Sleeps <sleeps@trufflemonkey.co.uk>
2026-05-28 13:02:49 +0100

    Removed insecure old web interface

--- vexim/config/i18n.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$language = 'en_US';
-putenv ("LANG=$language");
-setlocale(LC_ALL, "");
-bindtextdomain('messages', './locale');
-textdomain('messages');
-
-?>
--- vexim/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once dirname(__FILE__) . '/config/httpheaders.php';
-    include_once dirname(__FILE__) . "/config/functions.php";
-    invalidate_session();
-    header ('Location: index.php');
-?>
-<!-- Layout and CSS tricks obtained from http://www.bluerobot.com/web/layouts/ -->
